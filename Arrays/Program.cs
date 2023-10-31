46b1382e15f2b69965af6ddd01366554fe019643
Charlie <charlieryancurry@gmail.com>
2023-10-31 15:42:44 -0300

    add weapons array

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -12,24 +12,62 @@ namespace Arrays
     {
 
         static int[] ammo;
+        static string[] weapons;
         static int maxAmmo;
         static int currentWeapon;
 
         static void Main(string[] args)
         {
             ammo = new int[5];
+            weapons = new string[5];
             currentWeapon = 0;
-            maxAmmo = 100;
+            maxAmmo = 10;
 
-            for (int i = 0; i < ammo.Length; i++) 
+            for (int i = 0; i < ammo.Length; i++)
             {
                 ammo[i] = 10;
             }
+
+            weapons[0] = "Pistol";
+            weapons[1] = "Shotgun";
+            weapons[2] = "Spreader";
+            weapons[3] = "Rocket Launcher";
+            weapons[4] = "BFG";
 
             //Console.WriteLine("--------------");
             currentWeapon = 3;
             ShowHUD();
             Fire();
+            ShowHUD();
+            Fire();
+            ShowHUD();
+            Fire();
+            ShowHUD();
+            Fire();
+            ShowHUD();
+            Fire();
+            ShowHUD();
+            Fire();
+            ShowHUD();
+            Fire();
+            ShowHUD();
+            Fire();
+            ShowHUD();
+            Fire();
+            ShowHUD();
+            Fire();
+            ShowHUD();
+            Fire();
+            ShowHUD();
+            Fire();
+            ShowHUD();
+            Fire();
+            ShowHUD();
+            Fire();
+            ShowHUD();
+            Fire();
+            ShowHUD();
+            Reload();
             ShowHUD();
             Console.ReadLine();
         }
@@ -53,38 +91,13 @@ namespace Arrays
             ammo[currentWeapon] = maxAmmo;
         }
 
-        static string CurrentWeaponString()
-        {
-            if (currentWeapon == 0)
-            {
-                return "Pistol";
-            }
-            else if (currentWeapon == 1)
-            {
-                return "Shotgun";
-            }
-            else if (currentWeapon == 2)
-            {
-                return "Spreader";
-            }
-            else if (currentWeapon == 3)
-            {
-                return "Rocket Launcher";
-            }
-            else if (currentWeapon == 4)
-            {
-                return "BFG";
-            }
-            return "Pistol";
-        }
-
         static void ShowHUD()
         {
             Console.WriteLine();
             Console.WriteLine("WEAPON SYSTEM");
             Console.WriteLine("--------------");
-            Console.WriteLine("Current Weapon: " + CurrentWeaponString());
-            Console.WriteLine("Pistol: " + ammo[0] + " | " + "Shotgun: " + ammo[1] + " | " + "Spreader: " + ammo[2] + " | " + "Rocket Launcher: " + ammo[3] + " | " + "BFG: " + ammo[4]);
+            Console.WriteLine("Current Weapon: " + weapons[currentWeapon]);
+            Console.WriteLine(weapons[0] + ": " + ammo[0] + " | " + weapons[1] + ": " + ammo[1] + " | " + weapons[2] + ": " + ammo[2] + " | " + weapons[3] + ": " + ammo[3] + " | " + weapons[4] + ": " + ammo[4]);
         }
     }
 }
